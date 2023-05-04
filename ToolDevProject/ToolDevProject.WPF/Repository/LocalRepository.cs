0047db536dfa2be71503065e7604f6159cf64fb1
Simon Schaep <simon.schaep@gmail.com>
2023-05-04 21:41:46 +0200

    created repository

---
+++ ToolDevProject/ToolDevProject.WPF/Repository/LocalRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using ToolDevProject.WPF.Model;
+
+namespace ToolDevProject.WPF.Repository
+{
+    internal class LocalRepository
+    {
+        private static List<Hero> _heroStats;
+
+        public static List<Hero> GetHeroStats()
+        {
+            
+
+            return _heroStats;
+        }
+    }
+}
