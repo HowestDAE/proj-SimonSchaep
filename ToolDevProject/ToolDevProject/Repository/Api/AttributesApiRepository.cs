037786fa69c91d17612a06471f1c5e86462cc2cd
Simon Schaep <simon.schaep@gmail.com>
2023-06-07 14:46:49 +0200

    added await delay to get rid of warning

--- ToolDevProject/ToolDevProject/Repository/Api/AttributesApiRepository.cs
+++ ToolDevProject/ToolDevProject/Repository/Api/AttributesApiRepository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace ToolDevProject.WPF.Repository
 {
     //this data is currently not available in the api
-    //but if they ever add it, it should be easy to convert this code to make use of that
+    //but if they ever add it, it should be easy to just load that data here
     internal class AttributesApiRepository : IAttributesRepository
     {
         public int StrengthHealthGain { get; set; }
@@ -26,6 +26,7 @@ namespace ToolDevProject.WPF.Repository
 
         public async Task LoadAttributes()
         {
+            await Task.Delay(0); //to get rid of warning, remove when this data becomes available on the api and this repository can be implemented
             throw new NotImplementedException();
         }
     }
